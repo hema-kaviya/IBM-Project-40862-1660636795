0d212651841c48eed5091321ce3a7de0dce4c14f
PARAMESHWARAN <104810544+Vkparamesh@users.noreply.github.com>
2022-09-17 19:55:29 +0530

    python assaiment completed

---
+++ ASSAIMENTS/PARAMESHWARAN VK/Python-programs/odd numbers.py
@@ -0,0 +1,13 @@
+
+m = int(input("Enter the number to start: "));
+n = int(input("Enter the number to end: "));
+
+while(m<n):
+    if(m%2!=0):
+        print(m);
+    m=m+1;
+        
+    
+        
+        
+
