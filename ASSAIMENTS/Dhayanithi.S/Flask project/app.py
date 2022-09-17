58c33867c4d6e0d28edd035e19e87b7ffe44ce13
PARAMESHWARAN <104810544+Vkparamesh@users.noreply.github.com>
2022-09-17 21:00:09 +0530

    flask project completed

---
+++ ASSAIMENTS/Dhayanithi.S/Flask project/app.py
@@ -0,0 +1,24 @@
+from flask import Flask ,render_template,url_for,redirect,request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/success/<name>')
+def success(name):
+    return "welcome %s"%name
+
+@app.route("/login",methods =['POST','GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['input_name']
+        return redirect(url_for("success", name = user))
+    else:
+        user = request.args.get("input_name")
+        return redirect(url_for("success",name=user))
+
+
+if __name__ == '__main__':
+    app.run()
